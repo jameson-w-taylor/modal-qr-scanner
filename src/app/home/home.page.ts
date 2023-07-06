0dc68cec025cde8020c0890644c37ff34acb696a
Jameson Taylor <jameson@ionic.io>
2023-07-06 13:10:58 -0400

    Add sample

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Renderer2, ViewChild } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { IonModal } from '@ionic/angular';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,42 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonModal) modal?: IonModal;
+  scannerActive: boolean = false;
 
-  constructor() {}
+  constructor(@Inject(DOCUMENT) private document: Document, private renderer: Renderer2) {}
+
+  async onWillPresent() {
+    // NOTE: Make sure to always grant permission when testing this (only happy path is accounted for)
+    await BarcodeScanner.prepare();
+    await BarcodeScanner.checkPermission({ force: true });
+  }
+
+  async onDidPresent() {
+    // Set flag to hide content in parent page
+    this.scannerActive = true;
+    // Set <html /> background transparent
+    await BarcodeScanner.hideBackground();
+    // Set Ionic CSS variables to make Ionic component backgrounds transparent
+    this.renderer.addClass(this.document.body, 'scanner-active');
+    // Scan
+    const result = await BarcodeScanner.startScan();
+    if (result.hasContent) {
+      console.log(result.content);
+    }
+    this.modal?.dismiss(null, 'scanner');
+  }
+
+  async onWillDismiss() {
+    // Restore CSS styles and content
+    await BarcodeScanner.showBackground();
+    this.renderer.removeClass(this.document.body, 'scanner-active');
+    this.scannerActive = false;
+    await BarcodeScanner.stopScan();
+  }
+
+  cancel() {
+    this.modal?.dismiss(null, 'cancel');
+  }
 
 }
